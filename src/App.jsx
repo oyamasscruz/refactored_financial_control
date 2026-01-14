88dd72eaf559e5daf2df83a73be9dbc599db4c1c
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-01-14 18:14:40 -0300

    deleting the importing component that was deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import NewRelease from "./components/NewRelease";
 import ReleaseList from "./components/ReleaseList";
-import SelectedRelease from "./components/SelectedRelease";
 import Hero from "./components/Hero";
 
 function App() {
